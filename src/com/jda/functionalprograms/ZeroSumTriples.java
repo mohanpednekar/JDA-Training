25031f6fc199f7ebf88e00243a711fe1d80c2380
mohanpednekar <pednekar.mohan@gmail.com>
2018-07-06 14:38:50 +0530

    Test push to branch

--- src/com/jda/functionalprograms/ZeroSumTriples.java
+++ src/com/jda/functionalprograms/ZeroSumTriples.java
@@ -11,12 +11,22 @@ class ZeroSumTriples {
 		int n = utility.readInteger();
 		utility.printLine("Enter " + n + " integers");
 		int[] array = utility.readIntegerArray(n);
-		findZeroSumTriples(array);
+		int count = findZeroSumTriples(array);
+		utility.printLine("There are " + count + " zero sum triples");
 
 	}
 
 	private static
-	void findZeroSumTriples(int[] array) {
-
+	int findZeroSumTriples(int[] array) {
+		int n = array.length;
+		int count = 0;
+		for (int i = 0; i < n - 2; i++)
+			for (int j = i + 1; j < n - 1; j++)
+				for (int k = j + 1; k < n; k++)
+					if (array[i] + array[j] + array[k] == 0) {
+						count++;
+						System.out.println(array[i] + " " + array[j] + " " + array[k]);
+					}
+		return count;
 	}
 }
