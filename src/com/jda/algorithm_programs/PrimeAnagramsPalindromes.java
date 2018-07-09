8e592f3cd69666498e5d4873c5d99316733ad46b
Mohan <pednekar.mohan@gmail.com>
2018-07-09 17:58:32 +0530

    Anagrams and Palindromes completed

--- src/com/jda/algorithm_programs/PrimeAnagramsPalindromes.java
+++ src/com/jda/algorithm_programs/PrimeAnagramsPalindromes.java
@@ -11,11 +11,8 @@ import java.util.ArrayList;
 
 import com.jda.utility.Util;
 
-/**
- * @author bridgelabz
- */
 public class PrimeAnagramsPalindromes {
-
+	
 	/**
 	 * @param args
 	 */
@@ -23,7 +20,16 @@ public class PrimeAnagramsPalindromes {
 		// TODO Auto-generated method stub
 		int maxPrime = 1000;
 		ArrayList<Integer> primes = Util.primesUpto(maxPrime);
-
+		primes.forEach(prime -> {
+			if (Util.isPalindrome(String.valueOf(prime))) System.out.print(prime + " \t");
+		});
+		System.out.println();
+		for (Integer prime1 : primes)
+			for (Integer prime2 : primes) {
+				if (prime1 >= prime2) continue;
+				if (Util.areAnagrams(String.valueOf(prime1), String.valueOf(prime2)))
+				    System.out.print(prime1 + " and " + prime2 + " \t");
+			}
 	}
-
+	
 }
