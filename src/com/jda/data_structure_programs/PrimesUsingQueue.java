c3fbf704131c34418a0b6aa18373ee81e3b039ed
mohanpednekar <pednekar.mohan@gmail.com>
2018-07-20 18:11:03 +0530

    Anagram Primes using Stack done

---
+++ src/com/jda/data_structure_programs/PrimesUsingQueue.java
@@ -0,0 +1,20 @@
+package com.jda.data_structure_programs;
+
+import com.jda.utility.Queue;
+import com.jda.utility.Util;
+import java.util.ArrayList;
+
+public
+class PrimesUsingQueue {
+
+  public static
+  void main(String[] args) {
+    ArrayList<Integer> primes = Util.primesUpto(1000);
+    Queue<Integer> primesQueue = new Queue<>();
+    for (Integer prime : primes) {
+      if (Util.isPrimeAnagram(primes, prime)) { primesQueue.enqueue(prime); }
+    }
+
+    while (!primesQueue.isEmpty()) { System.out.println(primesQueue.dequeue()); }
+  }
+}
